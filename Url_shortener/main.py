f3e1041486a7145644ce05d042dee80d047712f2
Ayarina <ayat.harastani@gmail.com>
2020-11-18 19:43:28 +0100

    intento5

--- Url_shortener/main.py
+++ Url_shortener/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from typing import Optional
+
+import requests
+
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message" : "owo"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
