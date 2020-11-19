db23d672240594a99ba12d1b5c7ffeb6c258c338
Ayarina <ayat.harastani@gmail.com>
2020-11-19 22:07:37 +0100

    "Diccionario"

--- Url_shortener/main.py
+++ Url_shortener/main.py
@@ -11,17 +11,10 @@ app = FastAPI(
     version="69.1",
 )
 
-dic = []
+dic = {}
 
-lista = {
-    "sample" : "sample.com"
-}
 class Link(BaseModel):
     url: str
-
-class Short(BaseModel):
-    url: str
-    shortened_url: str
 
 @app.get("/")
 async def root():
@@ -31,26 +24,20 @@ async def root():
 async def list_url():
     return dic
     
-#@app.get("/url/{url_id}")
-#async def list_url(item: Link):
-#    return { "url" : item.url}
-
 @app.post("/url/")
 async def shorten_url(item: Link):
     if item.url in dic:
-        return {"url" : item.url, "shortened_url" : d[item.url]}
-    else
+        return {"url" : item.url, "shortened_url" : dic[item.url]}
+    else:
         shortened_url = random_url(item.url)
-        dic.append(item, shortened_url)
-        return {"url" : item.url, "shortened_url" : shortened_url}
+        dic[item.url] = shortened_url
+        #dic.append(item)
+        return {"url" : item.url, "shortened_url" : shortened_url}    
 
 
 def random_url(url: str):
     letters = string.ascii_letters
-    length = 5
-    while len(dic) < length*26*2:
-        length = length + 1
-
+    length = 6
     result = 'https://www.UrlDecapitator.com/'.join(random.choice(letters) for i in range(length))
     return result
 
