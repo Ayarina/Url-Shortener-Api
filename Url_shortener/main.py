9139f080cc2c05ae6da136ce37299334285146e1
Ayarina <ayat.harastani@gmail.com>
2020-12-07 16:02:45 +0100

    FastAPI Simple OAuth2 with Password and Bearer

--- Url_shortener/main.py
+++ Url_shortener/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
 from pydantic import BaseModel
 from typing import Optional
 
 from starlette.responses import RedirectResponse
 
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI(
     title="URL Beheader",
@@ -11,9 +14,29 @@ app = FastAPI(
     version="69.1",
 )
 
+some_users = {
+    "zanahoria":{
+        "username": "zanahoria",
+        "hashed_password": "hashedcocida",
+    },
+    "guisante":{
+        "username": "guisante",
+        "hashed_password": "hashedmuyrico",
+    },
+}
+
 db = []
 takenu = []
 takenc = []
+
+class User(BaseModel):
+    username: str
+
+class UserInDB(User):
+    hashed_password: str
+
+def fake_hash_password(password: str):
+    return "hashed" + password
 
 class Link(BaseModel):
     url: str
@@ -24,11 +47,11 @@ async def root():
     return {"message" : "Decapitador de Urls. Bienvenido sea."}
 
 @app.get("/url/")
-async def list_url():
+async def list_url(token: str = Depends(oauth2_scheme)):
     return db
 
 @app.post("/")
-async def shorten_url(item: Link):
+async def shorten_url(item: Link, token: str = Depends(oauth2_scheme)):
     url = item.url
     custom = item.custom_name
     if {url: custom} in db:
@@ -55,3 +78,18 @@ async def redirect(short: str):
         if short in takenc[i]:
             return RedirectResponse(takenu[i])
     return {"message": "URL not defined in the database"}
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = some_users.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    
+    return {"acces_token": user.username, "token_type": "bearer"}
+
+
+
